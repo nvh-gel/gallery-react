9d9e019a3e8d9ffc9c77fd4443fb6f0f9c1a56fb
nvh-gel <nvhien2703@outlook.com>
2023-04-12 15:50:26 +0700

    apply theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,84 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import PageMenu from "./components/menu";
-import {Layout} from "antd";
+import {ConfigProvider, Layout} from "antd";
 import {Content, Footer, Header} from "antd/es/layout/layout";
-import {HashRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes, useLocation} from "react-router-dom";
 import PageFooter from "./components/footer";
 import HomePage from "./pages/home";
 import GalleryPage from "./pages/gallery";
 import AboutPage from "./pages/about";
 import ContactPage from "./pages/contact";
 
+const themeBlue = {
+    token: {
+        colorBgBase: '#74ccee',
+    }
+}
+const themeRed = {
+    token: {
+        colorBgBase: '#ed544c',
+    }
+}
+const themeYellow = {
+    token: {
+        colorBgBase: '#eaa319',
+    }
+}
+const themeGreen = {
+    token: {
+        colorBgBase: '#16b5a3',
+    }
+}
+
 function Page() {
+
+    const location = useLocation();
+    const [currentTheme, setCurrentTheme] = useState(themeBlue);
+
+    function defineTheme(pathname: string) {
+        switch (pathname) {
+            case "/":
+                return themeBlue;
+            case "/gallery":
+                return themeRed;
+            case "/about":
+                return themeYellow;
+            case "/contact":
+                return themeGreen;
+            default:
+                return themeBlue;
+        }
+    }
+
+    useEffect(() => {
+        setCurrentTheme(defineTheme(location.pathname));
+    }, [location]);
+
     return (
         <div className="container">
-            <Layout className="app layout">
-                <Header className="layout-header">
-                    <PageMenu/>
-                </Header>
-                <Content className="content">
-                    <Routes>
-                        <Route path="/" key="home" element={<HomePage/>}/>
-                        <Route path="/gallery" key="gallery" element={<GalleryPage/>}/>
-                        <Route path="/about" key="about" element={<AboutPage/>}/>
-                        <Route path="/contact" key="contact" element={<ContactPage/>}/>
-                    </Routes>
-                </Content>
-                <Footer>
-                    <PageFooter/>
-                </Footer>
-            </Layout>
+            <ConfigProvider theme={currentTheme}>
+                <Layout className="app layout">
+                    <Header className="layout-header">
+                        <PageMenu/>
+                    </Header>
+                    <Content className="content">
+                        <Routes>
+                            <Route path="/" key="home" element={<HomePage/>}/>
+                            <Route path="/gallery" key="gallery" element={<GalleryPage/>}/>
+                            <Route path="/about" key="about" element={<AboutPage/>}/>
+                            <Route path="/contact" key="contact" element={<ContactPage/>}/>
+                        </Routes>
+                    </Content>
+                    <Footer>
+                        <PageFooter/>
+                    </Footer>
+
+                </Layout>
+            </ConfigProvider>
         </div>
-    );
+    )
+        ;
 }
 
 const App: React.FC = () => (
--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -7,15 +7,19 @@ import {NavLink, useLocation} from "react-router-dom";
 const menuItems = [{
     key: '/',
     label: <NavLink to="/">Home</NavLink>,
+    color: '#74ccee',
 }, {
     key: '/gallery',
     label: <NavLink to="/gallery">Gallery</NavLink>,
+    color: '#ed544c',
 }, {
     key: '/about',
     label: <NavLink to="/about">About Us</NavLink>,
+    color: '#eaa319',
 }, {
     key: '/contact',
     label: <NavLink to="/contact">Contact</NavLink>,
+    color: '#16b5a3',
 },];
 
 function PageMenu() {
@@ -23,8 +27,7 @@ function PageMenu() {
     const location = useLocation();
 
     return (
-        <Row justify="space-between" className="nav-bar" align="middle">
-            <Col span={4}/>
+        <Row justify="space-around" className="nav-bar" align="bottom">
             <Col span={8}>
                 <NavLink to="/">
                     <Space direction="horizontal">
@@ -33,12 +36,13 @@ function PageMenu() {
                     </Space>
                 </NavLink>
             </Col>
-            <Col span={4}/>
             <Col span={8}>
-                <Menu className="menu nav-bar"
-                      mode="horizontal"
-                      items={menuItems}
-                      selectedKeys={[location.pathname]}
+                <Menu
+                    className="nav-bar"
+                    mode="horizontal"
+                    items={menuItems}
+                    selectedKeys={[location.pathname]}
+                    style={{float: "right"}}
                 />
             </Col>
         </Row>
