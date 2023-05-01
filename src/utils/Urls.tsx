ae936b9b06cef8c4dc29a55a6226a27b728f3156
nvh-gel <nvhien2703@outlook.com>
2023-05-01 13:08:18 +0700

    add file urls

---
+++ src/utils/Urls.tsx
@@ -0,0 +1,11 @@
+export const Urls = {
+    BASE: `${process.env.REACT_APP_BASE_URL}`,
+    AUTH: '/auth',
+    LOGIN: '/login',
+    MODEL: '/model',
+    CRAWL_MODEL: '/crawl/model',
+    MOVE: '/move',
+    SKIP: '/skip',
+    SEARCH: '/search',
+    LINK: '/link',
+};
